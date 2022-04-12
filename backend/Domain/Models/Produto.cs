585d2b3921a6d9ceedc8842c77fd969775c2b8f9
Nicolas <nicolasbuscarini@hotmail.com>
2022-04-12 17:15:43 -0300

    mudanca de diretorio

---
+++ backend/Domain/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace MarketPlace.Domain.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public decimal Preco { get; private set; }
+        public int Estoque { get; private set; }
+
+        public Produto( string nome, string descricao, decimal preco, int estoque )
+        {
+            Nome = nome ?? throw new ArgumentNullException(nameof(nome));
+            Descricao = descricao ?? throw new ArgumentNullException(nameof(descricao));
+            Preco = preco;
+            Estoque = estoque;
+        }
+    }
+}
