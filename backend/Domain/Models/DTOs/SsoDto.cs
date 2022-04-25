7ee6bb4727de622129b955e02deb0df3619227c6
Nicolas <nicolasbuscarini@hotmail.com>
2022-04-25 10:39:12 -0300

    SsoDto.cs

--- backend/Domain/Models/DTOs/SsoDto.cs
+++ backend/Domain/Models/DTOs/SsoDto.cs
@@ -1,16 +1,17 @@
-﻿namespace MarketPlace.Domain.Models.DTOs;
-
-public class SsoDto
+﻿namespace MarketPlace.Domain.Models.DTOs
 {
-    public string Access_token { get; set; }
-    public DayOfWeek Expiration { get; set; }
-    public ApplicationUser User { get; set; }
-
-    public SsoDto(string access_token, DateTime expiration, ApplicationUser user)
+    public class SsoDto
     {
+        public string Access_token { get; set; }
+        public DayOfWeek Expiration { get; set; }
+        public ApplicationUser User { get; set; }
 
-        this.Access_token = access_token;
-        this.Expiration = DateTime.Now.DayOfWeek;
-        this.User = user;
+        public SsoDto(string access_token, DateTime expiration, ApplicationUser user)
+        {
+
+            this.Access_token = access_token;
+            this.Expiration = DateTime.Now.DayOfWeek;
+            this.User = user;
+        }
     }
 }
