6d4a842d1feb75c254884672ba4bb88b6b9d1fdc
nicolasbuscarini <nicolasbuscarini@hotmail.com>
2022-04-29 14:59:41 +0000

    deploy

--- backend/Impl/Service/DatabaseManagementService.cs
+++ backend/Impl/Service/DatabaseManagementService.cs
@@ -1,20 +1,26 @@
 using MarketPlace.Infrastructure.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MarketPlace.Impl.Service;
 
 public static class DatabaseManagementService
     {
-         
         public static void MigrationInitialisation(IApplicationBuilder app)
         {
             Console.WriteLine("11111111");
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
                 Console.WriteLine("22222222");
-                serviceScope.ServiceProvider.GetService<MySqlContext>().Database.Migrate();
+                var serviceMySql = serviceScope.ServiceProvider.GetService<MySqlContext>();
+                
+                var migrator = serviceMySql.GetInfrastructure().GetService<IMigrator>();
+
+                migrator.Migrate();
+                
                 Console.WriteLine("3333333333");
             }
         }
