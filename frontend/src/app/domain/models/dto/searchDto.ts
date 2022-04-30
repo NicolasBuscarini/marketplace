90cdcda240aa7d05f5e344ae15ea1017ad734edf
Nicolas <nicolasbuscarini@hotmail.com>
2022-04-30 00:23:24 -0300

    pesquisa de produtos no front

---
+++ frontend/src/app/domain/models/dto/searchDto.ts
@@ -0,0 +1,12 @@
+export class SearchDto {
+
+  pageNumber: number;
+  searchString: string;
+  pageSize: number;
+
+  constructor(pageNumber: number, searchString: string, pageSize: number) {
+    this.pageNumber = pageNumber;
+    this.searchString = searchString;
+    this.pageSize = pageSize;
+  }
+}
